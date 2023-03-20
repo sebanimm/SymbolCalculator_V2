3b01d0665dcfd2c2de922d598c827292a21d0928
sebanimm <102154824+sebanimm@users.noreply.github.com>
2023-03-20 15:11:01 +0900

    UPDATE : Add result state and button click handler

--- src/App.tsx
+++ src/App.tsx
@@ -14,31 +14,36 @@ const App = () => {
 		symbolLevel: 1,
 		symbolCount: 0,
 	});
-	const [isClicked, setIsClicked] = useState<Boolean>(false);
-
+	const [result, setResult] = useState<any>({
+		requiredCount: 0,
+		requiredCostIn100M: 0,
+		requiredCostIn10K: 0,
+	});
 	const { symbolLevel, symbolCount } = inputValues;
 	const { symbolType, baseCost, additionalCost } = symbolInfo;
-
-	const requiredCount = SymbolCalculator.calculateRequiredSymbolCount({
-		symbolType,
-		symbolLevel,
-		symbolCount,
-	});
-	const requiredCost = SymbolCalculator.calculateRequiredCost({
-		symbolType,
-		symbolLevel,
-		baseCost,
-		additionalCost,
-	});
-	const requiredCostIn100M = Math.floor(requiredCost / 100_000_000);
-	const requiredCostIn10K = Math.floor((requiredCost % 100_000_000) / 10000);
 
 	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
 		const { name, value, maxLength } = e.target;
 		const parsedValue = Number(value);
 		if (value.length > maxLength || isNaN(parsedValue)) return;
 		setInputValues({ ...inputValues, [name]: parsedValue });
-		setIsClicked(false);
+	};
+
+	const handleButtonClick = () => {
+		const requiredCount = SymbolCalculator.calculateRequiredSymbolCount({
+			symbolType,
+			symbolLevel,
+			symbolCount,
+		});
+		const requiredCost = SymbolCalculator.calculateRequiredCost({
+			symbolType,
+			symbolLevel,
+			baseCost,
+			additionalCost,
+		});
+		const requiredCostIn100M = Math.floor(requiredCost / 100_000_000);
+		const requiredCostIn10K = Math.floor((requiredCost % 100_000_000) / 10000);
+		setResult({ requiredCount, requiredCostIn100M, requiredCostIn10K });
 	};
 
 	return (
@@ -72,31 +77,15 @@ const App = () => {
 				onChange={handleChange}
 			/>
 			<br />
-			<button onClick={() => setIsClicked(true)}>계산하기</button>
+			<button onClick={handleButtonClick}>계산하기</button>
 			<br />
 			<br />
-			{isClicked && (
-				<div>
-					{requiredCount <= 0 || symbolCount < 0 || symbolLevel <= 0 ? (
-						"심볼 범위 초과함"
-					) : symbolType === DEFAULT ? (
-						"심볼고르셈"
-					) : (
-						<div>
-							{`${requiredCount}개 남음`}
-							<br />
-							{baseCost === 1 ? (
-								`심볼 선택하셈`
-							) : (
-								<>
-									{requiredCostIn100M === 0 ? "" : `${requiredCostIn100M}억`}
-									{`${requiredCostIn10K}만 메소 필요함`}
-								</>
-							)}
-						</div>
-					)}
-				</div>
-			)}
+			<div>
+				심볼 {result.requiredCount}개 더 필요함
+				<br />
+				{result.requiredCostIn100M}억{result.requiredCostIn10K}만 메소 더 필요함
+				<br />
+			</div>
 		</div>
 	);
 };
